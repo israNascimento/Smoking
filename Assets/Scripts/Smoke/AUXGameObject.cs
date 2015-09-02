a0c41a0909fffe1c7be8ed3b0227d6bca1df60f7
israNascimento <israelnascimento.nave@gmail.com>
2015-09-02 10:54:47 -0300

    Fumaça feita

---
+++ Assets/Scripts/Smoke/AUXGameObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AUXGameObject : MonoBehaviour {
+	private float speedX, speedY;
+	void Start () 
+	{
+		StartCoroutine (changeSpeed ());
+	}
+	
+	void Update ()
+	{
+		this.gameObject.transform.position += new Vector3 (0.001f, speedY);
+	}
+
+	IEnumerator changeSpeed()
+	{
+		while (true) 
+		{
+			yield return new WaitForSeconds(1);
+			Random r = new Random();
+			//speedX = Random.Range(0.00001f, 0.002f);
+			speedY = Random.Range(-0.002f, 0.002f);
+		}
+	}
+}
---
+++ Assets/Scripts/Smoke/Smoke.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smoke : MonoBehaviour 
+{
+	public GameObject rotation;
+	void Start ()
+	{
+
+	}
+	
+	void Update () 
+	{
+		gameObject.transform.RotateAround (rotation.transform.position, Vector3.forward, 200 * Time.deltaTime);
+	}
+}
